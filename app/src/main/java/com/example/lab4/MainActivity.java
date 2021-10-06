bb9dac44bfe9f7ba15891038a09556509c4e268a
Aman Manazir <amanazir@modern-woodmen.org>
2021-10-06 15:29:31 -0500

    Milestone 1 Complete

---
+++ app/src/main/java/com/example/lab4/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.lab4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private Button startButton;
+    private volatile boolean stopThread = false;
+    private TextView progress;
+
+    public void mockFileDownloader() {
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                startButton.setText("Downloading...");
+            }
+        });
+
+        progress = findViewById(R.id.progress);
+
+        for (int downloadProgress = 0; downloadProgress <= 100; downloadProgress = downloadProgress+10) {
+
+            if (stopThread) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        startButton.setText("Start");
+                    }
+                });
+                return;
+            }
+            Log.d(TAG, "Download Progress: " + downloadProgress + "%");
+            int finalDownloadProgress = downloadProgress;
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    progress.setText("Download Progress: " + finalDownloadProgress + "%");
+                }
+            });
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    class ExampleRunnable implements Runnable {
+        @Override
+        public void run() {
+            mockFileDownloader();
+        }
+    }
+    public void startDownload(View view) {
+        stopThread = false;
+        ExampleRunnable runnable = new ExampleRunnable();
+        new Thread(runnable).start();
+
+
+    }
+
+    public void stopDownload(View view) {
+        stopThread = true;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        startButton = findViewById(R.id.startButton);
+    }
+}
